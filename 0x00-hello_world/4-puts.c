62492c55bf0408dc612c857c9dcb1e877cf049a5
Chareeef <youssef.charif.h@gmail.com>
2023-03-16 11:55:35 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a beautiful sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
