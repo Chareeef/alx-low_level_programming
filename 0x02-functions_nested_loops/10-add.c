cb0d6c1f8e659d6da920845969caed0e4f9c6bca
Chareeef <youssef.charif.h@gmail.com>
2023-03-21 20:38:39 +0000

    t10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ * @n: First member
+ * @m: Second member
+ *
+ * Return: The sum
+ */
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+
+}
+
