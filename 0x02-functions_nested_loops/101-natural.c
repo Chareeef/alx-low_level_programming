060262bc1d665b9f0546dd4a40b5bf3550651f9b
Chareeef <youssef.charif.h@gmail.com>
2023-03-22 12:57:44 +0000

    t101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,15 @@
 #include "main.h"
 
 /**
- * sum_mult - Prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * sum_mult - Prints the sum of all the multiples of 3 or 5 below 1024
  *
  * Return: void
  */
 void sum_mult(void)
 {
-	// Init int n to 0
-	// FOR i = 0; i < 10...
-	// Search i mult of 3 or 5 from 0 to 1024 (strictely)
-	// Add i to previous n
-	// ENDFOR
-	// Print n
-	
-	int n = 0;
+	int n = 0, i;
 
-	for (i = 0; i < 1024; o++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			n += i;
