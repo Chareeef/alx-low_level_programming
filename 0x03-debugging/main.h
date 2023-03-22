017b44e46146ac6b81779f6d78a4283377f9705f
Chareeef <youssef.charif.h@gmail.com>
2023-03-22 21:27:53 +0000

    t3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
-convert
-prin
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
