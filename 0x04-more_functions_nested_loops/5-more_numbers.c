6b1d32fc31812e1038584bb7f0df50a5ad9c23cd
Chareeef <youssef.charif.h@gmail.com>
2023-03-23 11:43:13 +0000

    t5 fic betty

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - Prints 10 times the numbers, from 0 to 14.
  *
  * Return: voiđ
  */
