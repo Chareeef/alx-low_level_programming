640be4b8b5b30a8c11d677dbf67f2ba563dfdb27
Chareeef <youssef.charif.h@gmail.com>
2023-03-23 11:41:22 +0000

    t5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,17 @@
  */
 void more_numbers(void)
 {
-	int c;
+	int c, i;
 
-	for (c = 0; c <= 14; c++)
+	for (i = 0; i < 10; i++)
 	{
-		if (c / 10 != 0)
-			_putchar(c / 10 + '0');
-		_putchar(c % 10 + '0');
+		for (c = 0; c <= 14; c++)
+		{
+			if (c / 10 != 0)
+				_putchar(c / 10 + '0');
+			_putchar(c % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
