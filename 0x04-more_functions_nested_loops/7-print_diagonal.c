60f7895c4ebcea48034af7ea71a0a27ab7ac2c59
Chareeef <youssef.charif.h@gmail.com>
2023-03-23 14:26:58 +0000

    t7 f.nl

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints '/' n times.
+ * @n: number of '/'
+ *
+ * Return: voiđ
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('/');
+		_putchar('\n');
+	}
+}
+
