a0b9ad141e0706de8dc11e482ead67a8b7ecbf21
Chareeef <youssef.charif.h@gmail.com>
2023-03-23 17:32:21 +0000

    t9 FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * FizzBuzz - Prints Fizz for x3, Buzz for x5, FizzBuzz fot both.
+ * @n: number to check
+ *
+ * Return: void
+ */
+void FizzBuzz(int n)
+{
+	if (n % 3 == 0 && n % 5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (n % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (n % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+		printf("%d ", n);
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Succes.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		FizzBuzz(n);
+	}
+	printf("\n");
+
+	return (0);
+}
+
