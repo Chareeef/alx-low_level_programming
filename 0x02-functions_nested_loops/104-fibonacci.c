b76c52230197979ac4b36cd98765b09d87f42b8a
Chareeef <youssef.charif.h@gmail.com>
2023-03-25 23:28:57 +0000

    t104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,17 @@
 int main(void)
 {
 	unsigned long n = 1, head_n, tail_n, m = 2, head_m, tail_m;
-	unsigned long s = 0, d = 1000000000, hn, tn, ov;
+	unsigned long s = 0, d = 1000000000;
 	int i, balise = 0;
 
 	for (i = 0; i < 98; i++)
 	{
-		if (i == 92 && balise == 0)
+		if (i == 91 && balise == 0)
 		{
 			head_n = n / d;
 			tail_n = n % d;
 			head_m = m / d;
 			tail_m = m % d;
-			head_n = head_n + tail_n / d;
-			tail_n = tail_n % d;
-			head_m = head_m + tail_m / d;
-			tail_m = tail_m % d;
 			balise++;
 		}
 		if (balise == 0)
@@ -36,14 +32,12 @@ int main(void)
 		}
 		else
 		{
-			printf("%lu%lu", head_n, tail_n);
-			tn = (tail_n + tail_m) % d;
-			ov = (tail_n + tail_m) / d;
-			hn = head_n + head_m + ov;
+			printf("%lu", head_n + (tail_n / d));
+			printf("%lu", tail_n % d);
 			head_n = head_m;
+			head_m += head_n;
 			tail_n = tail_m;
-			head_m = hn;
-			tail_m = tn;
+			tail_m += tail_n;
 
 			if (i < 97)
 				printf(", ");
