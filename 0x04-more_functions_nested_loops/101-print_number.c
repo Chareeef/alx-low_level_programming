ac6fcc9c4d2799c3e752f8d6b79b460c97d3980a
Chareeef <youssef.charif.h@gmail.com>
2023-03-26 17:38:52 +0000

    num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * print_number - prints an integer with _putchar
@@ -10,23 +12,30 @@ void print_number(int n)
 {
 	int d = 1000000000, l = 0;
 
-	if (n < 0)
+	if (n < 0 && n != INT_MIN)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	while (l == 0 && n != 0)
+	while (l == 0 && n > 0)
 	{
 		l = (n / d) % 10;
 		d /= 10;
 	}
 
-	while (d > 0 && n != 0)
+	while (d > 0 && n > 0)
 	{
 		_putchar(l + '0');
 		l = (n / d) % 10;
 		d /= 10;
+	}
+
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		print_number((INT_MIN / 10000) * -1);
+		print_number((INT_MIN % 10000) * -1);
 	}
 	_putchar(n % 10 + '0');
 }
