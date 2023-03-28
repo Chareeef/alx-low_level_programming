bf903f35f185855ca9381e89e8e19e659cc9da04
Chareeef <youssef.charif.h@gmail.com>
2023-03-28 02:18:55 +0000

    t101

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,12 +9,12 @@
  */
 int _atoi(char *s)
 {
-	int n = 0, d = 1, i = 0;
+	int n = 0, d = 1, i = 0, m = 1;
 
 	while (s[i] < '0' || s[i] > '9')
 	{
 		if (s[i] == '-')
-			n = -n;
+			m = -m;
 		i++;
 	}
 
@@ -29,6 +29,8 @@ int _atoi(char *s)
 		d *= 10;
 		i -= 1;
 	}
+
+	n *= m;
 
 
 	return (n);
