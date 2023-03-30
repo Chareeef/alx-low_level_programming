14abf0e1bec4bf2e1ffec5feb3a1a2c4094d2d4b
Chareeef <youssef.charif.h@gmail.com>
2023-03-30 17:09:03 +0000

    rot13

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * rot13 - Encode a string using rot13
+ * infinite_add - Add two numbers stored in strings 
  * @s: Our string
  *
  * Return: the encoded string.
  */
-char *rot13(char *s)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j;
 	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -17,10 +17,7 @@ char *rot13(char *s)
 		for (j = 0; input[j] != '\0'; j++)
 		{
 			if (s[i] == input[j] && s[i - 1] != '\\')
-			{
 				s[i] = output[j];
-				break;
-			}
 		}
 	}
 
