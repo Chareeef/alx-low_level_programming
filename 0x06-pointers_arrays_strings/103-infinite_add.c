7d14e53d3bde5241c3597574dd19d36857b1470d
Chareeef <youssef.charif.h@gmail.com>
2023-03-31 02:14:46 +0000

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,9 +11,9 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, len1, j = 0, len2, k = 0, overflow = 0, sum, max = 0;
+	int i = 0, len1, j = 0, len2, k = 2, overflow = 0, sum, max = 0;
 
-	r[size_r] = '\0';
+	r[size_r - 1] = '\0';
 	if (((n1[0] - '0') + (n2[0] - '0')) > 9)
 		max = 1;
 
@@ -28,40 +28,44 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		max += len2;
 
-	if (max != size_r)
+	if (max > size_r)
 		return (0);
 
-	while (i >= 0 && j >= 0)
+	while (i >= 0 && j >= 0 && k <= size_r)
 	{
 		sum = (n1[i] - '0') + (n2[j] - '0') + overflow;
-		r[size_r - k - 1] = (sum % 10) + '0';
+		r[size_r - k] = (sum % 10) + '0';
 		overflow = sum / 10;
 		i--;
 		j--;
-		k--;
+		k++;
 	}
 
-	if (i == 0 && j == 0)
+	i++;
+	j++;
+	k--;
+
+	if (i == 0 && j == 0 && k <= size_r)
 	{
-		r[size_r - k - 1] = overflow + '0';
+		r[size_r - k] = overflow + '0';
 	}
 
-	while (j == 0 && i != 0)
+	while (j == 0 && i != 0 && k <= size_r)
 	{
 		sum = (n1[i] - '0') + overflow;
-		r[size_r - k - 1] = (sum % 10) + '0';
+		r[size_r - k] = (sum % 10) + '0';
 		overflow = sum / 10;
 		i--;
-		k--;
+		k++;
 	}
 
-	while (i == 0 && j != 0)
+	while (i == 0 && j != 0 && k <= size_r)
 	{
 		sum = (n2[j] - '0') + overflow;
-		r[size_r - k - 1] = (sum % 10) + '0';
+		r[size_r - k] = (sum % 10) + '0';
 		overflow = sum / 10;
 		j--;
-		k--;
+		k++;
 	}
 
 
