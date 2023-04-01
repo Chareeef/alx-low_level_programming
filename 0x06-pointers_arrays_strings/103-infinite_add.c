3180da5010ef28094a6e18eaee40b915c695068a
Chareeef <youssef.charif.h@gmail.com>
2023-04-01 14:18:31 +0000

    t103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * infinite_add - Add two numbers stored in strings 
+ * infinite_add - Add two numbers stored in strings
  * @n1: First string number
  * @n2: Second string number
  * @r: The result buffer
@@ -11,63 +12,45 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, len1, j = 0, len2, k = 2, overflow = 0, sum, max = 0;
+	int i = strlen(n1) - 1, j = strlen(n2) - 1;
+	int d1, d2, overflow = 0, sum, k = 0, l;
+	char temp;
 
-	r[size_r - 1] = '\0';
-	if (((n1[0] - '0') + (n2[0] - '0')) > 9)
-		max = 1;
+	/* Iterate n1 and n2 from right to left */
+	while (i >= 0 || j >= 0 || overflow)
+	{
+		d1 = i >= 0 ? n1[i] - '0' : 0;
+		d2 = j >= 0 ? n2[j] - '0' : 0;
+		sum = d1 + d2 + overflow;
+		overflow = sum / 10;
+		if (k < size_r)
+		{
+			r[k] = (sum % 10) + '0';
+			k++;
+		}
+		else
+		{
+			return (0);
+		}
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	len1 = i + 1;
-	len2 = j + 1;
-	if (len1 >= len2)
-		max += len1;
+		i--;
+		j--;
+	}
+
+	/* Reverse the result */
+	for (l = 0; l < k / 2; l++)
+	{
+		temp = r[l];
+		r[l] = r[k - l - 1];
+		r[k - l - 1] = temp;
+	}
+
+	/* Add '\0' to r */
+	if (k < size_r)
+		r[k] = '\0';
 	else
-		max += len2;
-
-	if (max > size_r)
 		return (0);
-
-	while (i >= 0 && j >= 0 && k <= size_r)
-	{
-		sum = (n1[i] - '0') + (n2[j] - '0') + overflow;
-		r[size_r - k] = (sum % 10) + '0';
-		overflow = sum / 10;
-		i--;
-		j--;
-		k++;
-	}
-
-	i++;
-	j++;
-	k--;
-
-	if (i == 0 && j == 0 && k <= size_r)
-	{
-		r[size_r - k] = overflow + '0';
-	}
-
-	while (j == 0 && i != 0 && k <= size_r)
-	{
-		sum = (n1[i] - '0') + overflow;
-		r[size_r - k] = (sum % 10) + '0';
-		overflow = sum / 10;
-		i--;
-		k++;
-	}
-
-	while (i == 0 && j != 0 && k <= size_r)
-	{
-		sum = (n2[j] - '0') + overflow;
-		r[size_r - k] = (sum % 10) + '0';
-		overflow = sum / 10;
-		j--;
-		k++;
-	}
-
 
 	return (r);
 }
+
--- 0x06-pointers_arrays_strings/prog.c
+++ 0x06-pointers_arrays_strings/prog.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h> 
-#include <limits.h>
+#include<limits.h>
 
 
 /**
@@ -10,6 +10,22 @@
  */
 int main(void)
 {
+	/*
+        char *n = "17";
+        char *m = "8";
+        char r[3];
+        char *res;
+
+        res = infinite_add(n, m, r, 3);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+	*/
         char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
         char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
         char r[100];
@@ -57,5 +73,6 @@ int main(void)
         {
                 printf("%s + %s = %s\n", n, m, res);
         }
+	/**/
         return (0);
 }
