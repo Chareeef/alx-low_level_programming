02a70d8cf4e71a122cf913c70029767195572467
Chareeef <youssef.charif.h@gmail.com>
2023-04-03 16:15:38 +0000

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chessboard 
+ * @a: 8x8 matrix
+ *
+ * Return: void.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (a[i][j] > 64)
+				putchar(a[i][j]);
+			else
+				putchar(' ');
+		}
+		putchar('\n');
+	}
+}
+
