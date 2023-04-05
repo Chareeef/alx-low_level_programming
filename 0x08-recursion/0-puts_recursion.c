818e7fbdf2b7e9dff130db853131627831d95dcf
Chareeef <youssef.charif.h@gmail.com>
2023-04-05 12:34:04 +0000

    t1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,9 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*(s + 1) != '\0')
+	if (*s != '\0')
+		_putchar(*s);
+	if (*(s + 1) != '\0' && *s != '\0')
 		_puts_recursion(s + 1);
 	else
 		_putchar('\n');
