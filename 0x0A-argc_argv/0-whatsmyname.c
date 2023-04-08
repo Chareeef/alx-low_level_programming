7f54f1231deea3c4f3cbca39e4f89ecdb1748b28
Chareeef <youssef.charif.h@gmail.com>
2023-04-08 18:17:43 +0000

    argc argv t0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print program's name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+int _putchar(char c);
+
+#endif
