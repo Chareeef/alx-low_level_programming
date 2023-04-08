24896535590e3663290a37490ccd79e6848b3ed3
Chareeef <youssef.charif.h@gmail.com>
2023-04-08 02:07:52 +0000

    test

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +20,3 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
