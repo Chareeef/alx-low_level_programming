3462920deed6046a2ec5139d42893d9195426ed3
Chareeef <youssef.charif.h@gmail.com>
2023-04-11 16:10:40 +0000

    allov grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates 2D array of integers
+ * @width: grid's columns
+ * @height: grid's rows
+ *
+ * Return: Pointer to grid, or NULL if any error.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	g = malloc(width * height * sizeof(int));
+	if (g == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			g[i][j] = 0;
+		}
+	}
+
+	return (g);
+}
