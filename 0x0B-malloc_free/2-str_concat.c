17dc6aa9586d458239837a7bfea70fac8d8e67f2
Chareeef <youssef.charif.h@gmail.com>
2023-04-11 15:45:21 +0000

    solve NULL problem

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	unsigned int i = 0, j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	s = (char *) malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
