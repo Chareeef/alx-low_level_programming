82d00d60fbf4b73828b4c9b247744730f9cfd111
Chareeef <youssef.charif.h@gmail.com>
2023-04-13 13:15:48 +0000

    t2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,27 @@
 #include "main.h"
 
 /**
- * malloc_checked - Allocate memory for array using malloc, and set it to 0
+ * _memset - Fill the first n bytes of s with b
+ * @s: Memory area to be filled
+ * @b: The byte to write into s
+ * @n: Bytes number to be filled
+ *
+ * Return: Pointer to s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - Allocate memory for array using malloc, and set it to 0
  * @nmemb: number of items
  * @size: bytes size for each item
  *
@@ -24,5 +44,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
+	_memset(a, 0, nmemb);
 	return (a);
 }
