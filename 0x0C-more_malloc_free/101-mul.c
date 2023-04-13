fcefd4df47b5478f777666f7c9e49d03b0f2f7b1
Chareeef <youssef.charif.h@gmail.com>
2023-04-13 17:31:59 +0000

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the product of two arguments
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: returns 0, or exits on 98 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (j = 0; i < 3; j++)
+	{
+		if (argv[i][j] == '\0')
+		{
+			i++;
+			j = 0;
+			continue;
+		}
+		if (argv[i][j] < '0' || argv[i][j] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	printf("%ld\n", (long) atoi(argv[1]) * (long) atoi(argv[2]));
+	return (EXIT_SUCCESS);
+}
