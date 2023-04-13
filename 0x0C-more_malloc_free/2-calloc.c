c0892cfb1d32e4dfdbeee4a6184bbef10570136e
Chareeef <youssef.charif.h@gmail.com>
2023-04-13 12:28:28 +0000

    t2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocate memory for array using malloc, and set it to 0
+ * @nmemb: number of items
+ * @size: bytes size for each item
+ *
+ * Return: a pointer to the allocated memory, returns NULL if an error occurs.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc(nmemb * size);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(a, 0, nmemb);
+	return (a);
+}
