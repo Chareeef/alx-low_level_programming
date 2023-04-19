056493ac1ff0837d0cd21470a53361cb781c46bb
Chareeef <youssef.charif.h@gmail.com>
2023-04-19 11:59:20 +0000

    function pointers Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name in a specific manner
+ * @name: name of the person
+ * @f: The function that prints name in its manner
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FP_H
+#define _FP_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
