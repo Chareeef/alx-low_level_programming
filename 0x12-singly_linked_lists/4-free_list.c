3f459e30d0da04c49d71472cc669c9766d6136ba
Chareeef <youssef.charif.h@gmail.com>
2023-04-27 17:34:08 +0100

    free list fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,16 +6,16 @@
  */
 void free_list(list_t *h)
 {
-	list_t *tracker = h;
+	list_t *tracker = NULL;
 
 	if (tracker == NULL)
 		return;
 
 	while (tracker->next != NULL)
 	{
+		tracker = h;
 		free(tracker->str);
-		free(tracker->next);
-		tracker = tracker->next;
+		free(tracker);
+		h = h->next;
 	}
-	free(h);
 }
