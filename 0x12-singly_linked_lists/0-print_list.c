aa5918b9616ca7a5631931985a7e214c1653dba7
Chareeef <youssef.charif.h@gmail.com>
2023-04-27 15:13:46 +0100

    Corresponding to task 0 format

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str != NULL)
-			printf("[%d] \"%s\"\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		h = h->next;
