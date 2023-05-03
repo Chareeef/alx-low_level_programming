852ba12f7632cd9e0a55558ceea0f7238db23343
Chareeef <youssef.charif.h@gmail.com>
2023-05-03 18:49:33 +0100

    test free_listint

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,37 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - safely free a singly linked list
+ * edge_free - free edge cases a singly linked list
  * @p_head: pointer to pointer to singly linked list head
+ *
+ * Return: freed size of the singly linked list.
+ */
+size_t edge_free(listint_t **p_head)
+{
+	size_t size = 0;
+	listint_t *head = *p_head,
+		  *temp = NULL;
+
+	if (head == head->next)
+	{
+		free(head);
+		size += sizeof(listint_t);
+		*p_head = NULL;
+	}
+	else if (head == head->next->next)
+	{
+		free(head->next);
+		free(head);
+		size += 2 * sizeof(listint_t);
+		*p_head = NULL;
+	}
+
+	return (size);
+}
+
+/**
+ * free_listint_safe - safely free a singly linked list
+ * @head: pointer to pointer to singly linked list head
  *
  * Return: freed size of the singly linked list.
  */
@@ -14,9 +43,11 @@ size_t free_listint_safe(listint_t **head)
 		  *temp  = NULL;
 
 	if (!head || !(*head))
-	{
 		return (0);
-	}
+
+	size = edge_free(head);
+	if (size > 0)
+		return (size);
 
 	while (poo && flash && flash->next)
 	{
@@ -31,11 +62,18 @@ size_t free_listint_safe(listint_t **head)
 				temp = poo;
 				poo = poo->next;
 				free(temp);
-				size++;
+				size += sizeof(listint_t);
 				flash = flash->next;
 			}
-			free(poo);
-			size++;
+			while (poo->next)
+			{
+				temp = poo;
+				poo = poo->next;
+				temp->next = NULL;
+				free(temp);
+				size += sizeof(listint_t);
+			}
+
 			*head = NULL;
 			return (size);
 		}
@@ -46,7 +84,7 @@ size_t free_listint_safe(listint_t **head)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		size++;
+		size += sizeof(listint_t);
 	}
 	*head = NULL;
 	return (size);
