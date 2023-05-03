ea451d655863f5a3f17c34e0aad36d74c239bab1
Chareeef <youssef.charif.h@gmail.com>
2023-05-03 15:42:07 +0100

    find_loop - 2nd try

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,31 +1,10 @@
 #include "lists.h"
 
 /**
- * find_loop_after_meeting - find loop in a singly linked list after meeting point
- * @flash: hare pointer
- * @poo:tortoise pointer
- *
- * Return: pointer to loop's start.
- */
-listint_t *find_loop_after_meeting(listint_t *flash, listint_t *poo)
-{
-	while (flash != poo)
-	{
-		flash = flash->next;
-		poo = poo->next;
-	}
-	if (flash == poo)
-	{
-		return (flash);
-	}
-	return (NULL);
-}
-
-/**
  * find_listint_loop - find loop in a singly linked list
  * @head: pointer to singly linked list head
  *
- * Return: pointer to loop's start.
+ * Return: pointer to loop's start, or NULL if it doesn't exist.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
@@ -34,20 +13,16 @@ listint_t *find_listint_loop(listint_t *head)
 	if (!head)
 	{
 		return (NULL);
-		exit(98);
 	}
-	else if (head == head->next)
+	else if (head == head->next || head == head->next->next)
 		return (head);
-	else if (head->next->next == head)
-		return (head->next);
 
 	while (1)
 	{
 		poo = poo->next;
-		if (!flash->next->next)
+		if (!flash->next->next || !flash->next)
 			return (NULL);
-		else
-			flash = flash->next->next;
+		flash = flash->next->next;
 
 		if (flash == poo)
 		{
@@ -58,7 +33,13 @@ listint_t *find_listint_loop(listint_t *head)
 
 	if (poo == head)
 	{
-		return (find_loop_after_meeting(flash, poo));
+		while (flash != poo)
+		{
+			flash = flash->next;
+			poo = poo->next;
+		}
+		if (flash == poo)
+			return (flash);
 	}
 
 	return (NULL);
