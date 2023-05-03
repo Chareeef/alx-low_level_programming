059320be804d33a144e8644ac74cfd56d2b377c3
Chareeef <youssef.charif.h@gmail.com>
2023-05-03 13:51:25 +0100

    print_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * print_edge - safely print edge cases of a singly linked list
+ * @head: pointer to singly linked list head
+ *
+ * Return: printed nodes number.
+ */
+size_t print_edge(const listint_t *head)
+{
+	size_t nodes = 0;
+
+	printf("[%p] %d\n", (void *) head, head->n);
+	nodes++;
+
+	if (head != head->next)
+	{
+		printf("[%p] %d\n", (void *) head->next, head->next->n);
+		nodes++;
+	}
+
+	printf("-> [%p] %d\n", (void *) head, head->n);
+	return (nodes);
+}
+
+/**
  * print_after_meeting - safely print a singly linked list after meeting point
  * @flash: hare pointer
  * @poo:tortoise pointer
@@ -21,7 +44,6 @@ size_t print_after_meeting(const listint_t *flash, const listint_t *poo,
 	if (flash == poo)
 	{
 		printf("-> [%p] %d\n", (void *) flash, flash->n);
-		(*nodes)++;
 	}
 	return (*nodes);
 }
@@ -44,13 +66,15 @@ size_t print_listint_safe(const listint_t *head)
 		return (nodes);
 		exit(98);
 	}
+	else if (head->next == head || head->next->next == head)
+		return (print_edge(head));
 
 	while (poo)
 	{
 		printf("[%p] %d\n", (void *) poo, poo->n);
 		nodes++;
 		poo = poo->next;
-		if (!poo || poo == head)
+		if (!poo)
 			return (nodes);
 		if (flash->next->next)
 			flash = flash->next->next;
