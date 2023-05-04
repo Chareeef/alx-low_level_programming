2980e80342ada6af0d99e52dd97ccf31fb359ed2
Chareeef <youssef.charif.h@gmail.com>
2023-05-04 16:46:04 +0100

    handle error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);
+	int bit;
+
+	if (index > sizeof(n) * 8)
+		return (-1);
+	bit = (n >> index) & 1;
+	return (bit);
 }
