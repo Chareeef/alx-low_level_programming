84ef1f9e019eab587a9fae4b2666442cfe63df94
Chareeef <youssef.charif.h@gmail.com>
2023-12-13 11:24:07 +0100

    Small arrays

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,18 +21,18 @@ int interpolation_search(int *array, size_t size, int value)
 	high = size - 1;
 	low = 0;
 
-	while (low <= high)
+	while (low <= high && high < size)
 	{
 		probe = low + (((double)(high - low) /
 				(array[high] - array[low])) * (value - array[low]));
 
 		if (probe < low || probe > high)
 		{
-			printf("Value checked array[%d] is out of range\n", (int) probe);
+			printf("Value checked array[%ld] is out of range\n", probe);
 			return (-1);
 		}
 
-		printf("Value checked array[%d] = [%d]\n", (int) probe, array[probe]);
+		printf("Value checked array[%ld] = [%d]\n", probe, array[probe]);
 		if (array[probe] == value)
 			return (probe);
 		else if (array[probe] < value)
