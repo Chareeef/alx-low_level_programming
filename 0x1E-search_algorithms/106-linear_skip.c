f4fcf43abb034fd035fc598ffb61dd1bb379f291
Chareeef <youssef.charif.h@gmail.com>
2023-12-13 19:24:41 +0100

    Add 106-linear_skip.c

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+
+skiplist_t *linear_search_sublist(skiplist_t *start,
+		skiplist_t *end, int value);
+
+/**
+ * linear_skip - searches for a value in an list of integers
+ * with an express lane using the jump search algorithm
+ * @head: pointer the head of the list to search in
+ * @value: the element to search for
+ *
+ * Return: the pointer to the first node where value is located,
+ * or NULL if value is not found.
+ */
+skiplist_t *linear_skip(skiplist_t *head, int value)
+{
+	skiplist_t *last, *current;
+
+	if (!head)
+		return (NULL);
+
+	last = head;
+
+	while (last->n <= value)
+	{
+		current = last;
+
+		if (last->express)
+			current = last->express;
+		else
+		{
+			while (current->next)
+			{
+				current = current->next;
+			}
+
+			printf("Value found between indexes [%ld] and [%ld]\n",
+					last->index, current->index);
+
+			return (linear_search_sublist(last, current, value));
+		}
+
+		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
+
+		if (current->n > value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+					last->index, current->index);
+			return (linear_search_sublist(last, current, value));
+		}
+		last = current;
+	}
+
+	return (NULL);
+}
+
+/**
+ * linear_search_sublist - searches for a value in a sub-.list
+ * of integers using the linear search algorithm
+ * @start: pointer to the first node of the sublist to search in
+ * @end: pointer to the last node of the sublist to search in
+ * @value: the element to search for
+ *
+ * Return: the pointer to the first node where value is located,
+ * or NULL if value is not found.
+ */
+skiplist_t *linear_search_sublist(skiplist_t *start,
+		skiplist_t *end, int value)
+{
+	skiplist_t *current;
+
+	if (!start || !end)
+		return (NULL);
+
+	current = start;
+
+	while (current)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
+
+		if (current->n == value)
+			return (current);
+
+		if (!current->next || current == end)
+			return (NULL);
+
+		current = current->next;
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -19,6 +19,24 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 /* Functions */
 
 int linear_search(int *array, size_t size, int value);
@@ -29,5 +47,6 @@ int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 
 listint_t *jump_list(listint_t *head, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
