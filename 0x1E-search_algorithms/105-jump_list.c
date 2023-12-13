292335e63b5cdcbc9253656555487934526988ef
Chareeef <youssef.charif.h@gmail.com>
2023-12-13 17:46:59 +0100

    Add 105-jump_list.c

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+
+listint_t *linear_search_sublist(listint_t *start, listint_t *end, int value);
+
+/**
+ * jump_list - searches for a value in an list
+ * of integers using the jump search algorithm
+ * @head: pointer the head of the list to search in
+ * @size: the list's number of elements
+ * @value: the element to search for
+ *
+ * Return: the pointer to the first node where value is located,
+ * or NULL if value is not found.
+ */
+listint_t *jump_list(listint_t *head, size_t size, int value)
+{
+	listint_t *last, *current;
+	int step, i;
+
+	if (!head)
+		return (NULL);
+
+	last = head;
+	step = sqrt(size);
+
+	while (last->n < value)
+	{
+		current = last;
+		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
+
+		for (i = 0; i < step; i++)
+		{
+			if (current->next)
+			{
+				current = current->next;
+			}
+			else
+			{
+				printf("Value found between indexes [%ld] and [%ld]\n",
+							last->index, current->index);
+				return (linear_search_sublist(last, current, value));
+			}
+		}
+		if (current->n > value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+					last->index, current->index);
+			return (linear_search_sublist(last, current, value));
+		}
+		last = current;
+	}
+
+	return (NULL);
+}
+
+/**
+ * linear_search_sublist - searches for a value in a sub-.list
+ * of integers using the linear search algorithm
+ * @start: pointer to the first node of the sublist to search in
+ * @end: pointer to the last node of the sublist to search in
+ * @value: the element to search for
+ *
+ * Return: the pointer to the first node where value is located,
+ * or NULL if value is not found.
+ */
+listint_t *linear_search_sublist(listint_t *start, listint_t *end, int value)
+{
+	listint_t *current;
+
+	if (!start || !end)
+		return (NULL);
+
+	for (current = start; current != end->next; current = current->next)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
+
+		if (current->n == value)
+			return (current);
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/* List Structure */
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 /* Functions */
 
 int linear_search(int *array, size_t size, int value);
@@ -9,5 +27,7 @@ int advanced_binary(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+
+listint_t *jump_list(listint_t *head, size_t size, int value);
 
 #endif
